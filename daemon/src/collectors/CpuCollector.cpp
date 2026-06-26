cdc936c9aa34292d71af46188d94eeda082b5e1a
TomasSalvador <tomassferreira2000@gmail.com>
2026-06-26 17:28:24 +0100

    restructure and rename some methods

--- daemon/src/collectors/CpuCollector.cpp
+++ daemon/src/collectors/CpuCollector.cpp
@@ -22,8 +22,29 @@ namespace {
         unsigned long idle_iowait;
     };
 
-    CpuInfo get_cpu_info(const std::string& cpu_info) {
-        std::istringstream cpu_info_stream(cpu_info);
+    std::optional<std::string> get_cpu_info_from_file(const std::string& file_path) {
+        // fecha a stream automaticamente quando a var que o referencia está fora do scope de uso (quando a funçao retorna).
+        std::ifstream file(file_path);
+
+        if (!file.is_open()) {
+            std::cerr << "Could not open the file in" << file_path << '\n';
+            return std::nullopt;
+        }
+
+        std::string line;    
+        std::getline(file, line);
+        
+        return line;
+    }
+
+    std::optional<CpuInfo> extract_cpu_info(const std::string& file_path) {
+        std::optional<std::string> cpu_info_string = get_cpu_info_from_file(file_path);
+        if (!cpu_info_string.has_value())
+        {
+            return std::nullopt;
+        }
+        
+        std::istringstream cpu_info_stream(*cpu_info_string);
 
         std::string label;
         cpu_info_stream >> label;
@@ -32,7 +53,7 @@ namespace {
         unsigned long idle_iowait = 0;
         unsigned long value = 0;
         
-        unsigned short idx = 0;
+        int idx = 0;
         while (cpu_info_stream >> value) {
             total += value;
             if (idx == IDLE_IDX || idx == IOWAIT_IDX)
@@ -46,33 +67,17 @@ namespace {
 
         return res;
     }
-
-    std::optional<CpuInfo> extract_cpu_usage_info(const std::string& file_name) {
-        // fecha a stream automaticamente quando a var que o referencia está fora do scope de uso (quando a funçao retorna).
-        std::ifstream file(file_name);
-
-        if (!file.is_open()) {
-            std::cerr << "Could not open the file in" << file_name << '\n';
-            return std::nullopt;
-        }
-
-        std::string line;    
-        std::getline(file, line);
-        CpuInfo cpu_info = get_cpu_info(line);
-
-        return cpu_info;
-    }
 }
 
 /**
  * Para inicializar campos no construtor, usar o ' : x' antes de '{}' para ficaram logo com o valor, se só for feito dentro do '{}' elas primeiro
  * sao inicializados com um valor default(depende do seu construtor) e só depois é que faz set ao valor que se quer.
  */
-CpuCollector::CpuCollector(const std::string& file_path) : file_path_(file_path) {};
+CpuCollector::CpuCollector(const std::string& file_path) : file_path_(file_path) {}
 
 
 std::optional<std::vector<Metric>> CpuCollector::collect() {
-    std::optional<CpuInfo> cpu_info1 = extract_cpu_usage_info(file_path_);
+    std::optional<CpuInfo> cpu_info1 = extract_cpu_info(file_path_);
 
     if (!cpu_info1.has_value())
     {
@@ -82,7 +87,7 @@ std::optional<std::vector<Metric>> CpuCollector::collect() {
 
     sleep(3);
 
-    std::optional<CpuInfo> cpu_info2 = extract_cpu_usage_info(file_path_);
+    std::optional<CpuInfo> cpu_info2 = extract_cpu_info(file_path_);
 
     if (!cpu_info2.has_value())
     {
@@ -102,8 +107,8 @@ std::optional<std::vector<Metric>> CpuCollector::collect() {
     double usage = (1 - static_cast<double>(idle_iowait_delta) / static_cast<double>(total_delta)) * 100.0;
 
     return std::vector<Metric>{{get_label(), "Usage", usage, "%"}}; 
-};
+}
 
 std::string CpuCollector::get_label() const {
     return "CPU";
-};
+}
